2e67fdad0763b473d93e6eed5c4323887355bdba
José Romo <a01197772@tec.mx>
2024-05-29 21:29:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 
+st.sidebar.header("Sidebar Title")
+st.sidebar.subheader("Subheading")
+st.sidebar.text("Sidebar content goes here.")
 
 st.write("""
          # Hola prueba cemex
