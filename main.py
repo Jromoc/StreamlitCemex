a9c0e14047fecb1dd0f2061083b0a456bc7aa714
José Romo <a01197772@tec.mx>
2024-05-29 21:26:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import streamlit as st
 st.set_page_config(layout="wide")
-# Custom HTML/CSS for the banner
-custom_html = """
-<div class="banner">
-    <img src="C:\Users\josee\OneDrive\Documents\7mo Concentracion\Bloque 2\Reto\StreamlitCemex\cemex logo.png" alt="Banner Image">
-</div>
-<style>
-    .banner {
-        width: 160%;
-        height: 200px;
-        overflow: hidden;
-    }
-    .banner img {
-        width: 100%;
-        object-fit: cover;
-    }
-</style>
-"""
+
 
 st.write("""
          # Hola prueba cemex
